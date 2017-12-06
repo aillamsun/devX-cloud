2a989762a43b014c4e0dbdceae6e310cc8287f75
孙刚 <1120sungang@gmail.com>
2017-12-06 15:21:42 +0800

    服务提供方

--- devx-provider-samples/user-service/src/main/java/com/devx/api/DevxUserServiceApp.java
+++ devx-provider-samples/user-service/src/main/java/com/devx/api/DevxUserServiceApp.java
@@ -1,7 +1,18 @@
 package com.devx.api;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * Created by sungang on 2017/12/6.
  */
+@SpringBootApplication
+@EnableEurekaClient
 public class DevxUserServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevxUserServiceApp.class, args);
+    }
+
 }
