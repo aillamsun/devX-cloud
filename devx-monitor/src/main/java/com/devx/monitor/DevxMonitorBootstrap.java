7c68d6ddd61b4fa0a39baa090313a78d0f0a6086
孙刚 <1120sungang@gmail.com>
2017-12-06 15:21:27 +0800

    监控

--- devx-monitor/src/main/java/com/devx/monitor/DevxMonitorBootstrap.java
+++ devx-monitor/src/main/java/com/devx/monitor/DevxMonitorBootstrap.java
@@ -4,13 +4,15 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 /**
  * Created by sungang on 2017/12/5.
  */
 @SpringBootApplication
-@EnableAdminServer
 @EnableEurekaClient
+@EnableAdminServer
+@EnableTurbineStream
 public class DevxMonitorBootstrap {
 
     public static void main(String[] args) {
